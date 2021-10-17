eb3be33de32faacb9d01ad95128244c4f21a39f4
Tom2612 <87855471+Tom2612@users.noreply.github.com>
2021-10-17 15:44:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,100 @@
-const topScreenText = document.querySelector('.top');
-topScreenText.textContent = '';
+let num1 = null;
+let num2 = null;
+let operator = null;
+let result = null;
+let clear = false;
+const displayBottom = document.querySelector('.bottom');
+const equal = document.querySelector('#equal');
+const numberButtons = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.operator');
 
-function add(a, b) {
-    return a + b;
-};
+function add(a, b) { return a + b };
+function subtract(a, b) { return a - b };
+function multiply(a, b) { return a * b };
+function divide(a, b) { return a / b };
 
-function subtract(a, b) {
-    return a - b;
-};
-
-function multiply(a, b) {
-    return a * b;
-};
-
-function divide(a, b) {
-    return a / b;
-};
-
-function operation(num1, operator, num2) {
-    if (operator === '+') {
-        return add(num1, num2);
-    } else if (operator === '-') {
-        return subtract(num1, num2);
-    } else if (operator === '*') {
-        return multiply(num1, num2);
+function operation(a, b, o) {
+    if (o === 'add') {
+        return add(a, b);
+    } else if (o === 'subtract') {
+        return subtract(a, b);
+    } else if (o === 'multiply') {
+        return multiply(a, b);
     } else {
-        return divide(num1, num2);
+        return divide(a, b);
     }
 };
 
-let display = document.querySelector('.bottom');
-display.textContent = '0000';
+function setDisplay(value) {
+    displayBottom.textContent = displayBottom.textContent + value;
+};
+
+//get the bottom display value
+function getDisplay() {
+    return displayBottom.textContent;
+};
+
+function clearDisplay() {
+    displayBottom.textContent = '';
+};
+
+//Assign num values
+function setNumbers(value) {
+    if (num1 == null) {
+        num1 = value;
+    } else {
+        num2 = value;
+    }
+};
+
+function setOperator(value) {
+    if (operator == null) {
+        operator = value;
+    } else if (num1 && num2) {
+        //do the current operation
+        result = operation(Number(num1), Number(num2), operator);
+        clearDisplay();
+        setDisplay(result);
+        num1 = result;
+        num2 = null;
+        operator = value;
+    }
+};
+
+function getResult() {
+    if (num1 && operator && !clear && !num2) {
+        setNumbers(getDisplay());
+        return operation(Number(num1), Number(num2), operator);
+    } else {
+        return false;
+    }
+};
+
+//Add number button listeners here
+numberButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (clear) {
+            clearDisplay();
+        }
+        setDisplay(e.target.textContent);
+        clear = false;
+    })
+});
+
+//Add store operation here
+operatorButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        setNumbers(getDisplay());
+        setOperator(e.target.id);
+        clear = true;
+    })
+});
+
+//Add equal input here
+equal.addEventListener('click', () => {
+    result = getResult();
+    clearDisplay();
+    if (result) {
+        setDisplay(result);
+    }
+});
